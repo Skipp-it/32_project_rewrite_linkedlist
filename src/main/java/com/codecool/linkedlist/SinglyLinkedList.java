97c505a9528eb917b565926709ac4158d1f23064
Celalalt666 <mihairaducroitoru@gmail.com>
2020-08-31 18:13:20 +0300

    all but one tests passed
    exit
    
    /
    /q
    :wq

--- src/main/java/com/codecool/linkedlist/SinglyLinkedList.java
+++ src/main/java/com/codecool/linkedlist/SinglyLinkedList.java
@@ -1,5 +1,7 @@
 package com.codecool.linkedlist;
 
+import javax.swing.plaf.IconUIResource;
+
 public class SinglyLinkedList {
 
     private class Link {
@@ -25,6 +27,7 @@ public class SinglyLinkedList {
     }
 
     private Link head;
+    private int addedTimes =0;
 
     public SinglyLinkedList() {
     }
@@ -36,7 +39,20 @@ public class SinglyLinkedList {
      *
      * @param value value to be appended
      */
+
     public void add(int value) {
+
+        if (head == null) {
+            this.head = new Link(value);
+//            addedTimes = 1;
+        } else {
+            Link temp = head;
+            while (temp.getNext() != null) {
+//                addedTimes++;
+                temp=temp.getNext();
+            }
+            temp.setNext(new Link(value));
+        }
     }
 
     /**
@@ -45,9 +61,30 @@ public class SinglyLinkedList {
      * @param index the position of requested value
      * @return value of element at index
      */
+
     public int get(int index) {
-        return 0;
+        Link temp = this.head;
+        if (this.size() <= index) {
+            throw new IndexOutOfBoundsException();
+        }
+        if (index == 0) {
+            if (temp == null) {
+                throw new IndexOutOfBoundsException();
+            }
+        } else if (index > 0) {
+            int count = 0;
+            while (temp.getNext() != null) {
+                count += 1;
+                temp = temp.getNext();
+                if (count == index) {
+                    return temp.getValue();
+                }
+            }
+        }
+        return temp.getValue();
     }
+
+
 
     /**
      * Returns the zero-based index of the first occurrence of a value in the list.
@@ -55,8 +92,22 @@ public class SinglyLinkedList {
      * @param number value to be searched
      * @return Index of 'number' if it's in the list, otherwise -1;
      */
+
     public int indexOf(int number) {
-        return 0;
+        int count = 0;
+        Link temp = this.head;
+        if (temp.getValue() == number) {
+            return 0;
+        }
+        while (temp.getNext() != null && temp.getValue() != number) {
+
+            count += 1;
+            temp = temp.getNext();
+            if (temp.getValue() == number) {
+                return count;
+            }
+        }
+        return -1;
     }
 
     /**
@@ -66,6 +117,33 @@ public class SinglyLinkedList {
      * @param number Value to be inserted.
      */
     public void insert(int index, int number) {
+        Link temp = head;
+        int count = 1;
+        if (index < 0) {
+            throw new IndexOutOfBoundsException();
+        }
+        else if (index == 0) {
+            head = new Link(number);
+            head.setNext(temp);
+        }
+        else if (index == this.size()) {
+            while (temp.getNext() != null) {
+                temp = temp.getNext();
+            }
+            temp.setNext(new Link(number));
+        }
+//        else if (index ==1){
+//            temp.setNext(new Link(number));
+//        }
+        else if (index > 0) {
+            while (temp.getNext() != null) {
+                if (index == count+1){
+                    temp.setNext(new Link(number));
+                }
+                count ++;
+                temp = temp.getNext();
+            }
+        }
     }
 
     /**
@@ -74,7 +152,23 @@ public class SinglyLinkedList {
      * @return Size of list.
      */
     public int size() {
-        return 0;
+        int countSize =0;
+        Link temp = this.head;
+        if (temp != null) {
+            countSize = 1;
+            while (temp.getNext() != null) {
+                countSize += 1;
+                temp = temp.getNext();
+            }
+        }
+        return countSize;
+    }
+
+    public void removeByValue(int value) {
+        int index = this.indexOf(value);
+        if (index != -1) {
+            this.remove(index);
+        }
     }
 
     /**
